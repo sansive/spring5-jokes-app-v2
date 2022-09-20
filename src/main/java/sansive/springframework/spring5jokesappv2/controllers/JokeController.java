21037a6dad9cd59666b5deb9772f49bb68ad17df
Sandra Sierra <sansive00@gmail.com>
2022-09-20 10:11:34 +0200

    Create Joke Controller

---
+++ src/main/java/sansive/springframework/spring5jokesappv2/controllers/JokeController.java
@@ -0,0 +1,25 @@
+package sansive.springframework.spring5jokesappv2.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import sansive.springframework.spring5jokesappv2.services.JokeService;
+
+@Controller
+public class JokeController {
+
+    private final JokeService jokeService;
+
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"/",""})
+    public String showJoke(Model model) {
+
+        model.addAttribute("joke", jokeService.getJoke());
+        
+        return "index";
+    }
+
+}
